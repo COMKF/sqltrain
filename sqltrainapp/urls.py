582be34f99ccc1d0d372846a1ddd99c6b4c71a6b
MengjieGuo <mjguocn@163.com>
2017-06-28 15:23:10 +0800

    add css,jquery && merge .gitingore

--- sqltrainapp/urls.py
+++ sqltrainapp/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
+from . import views
+
 
 app_name = 'sqltrainapp'
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^questions/basic/$', views.basic, name='basic'),
+    url(r'^questions/basic/selectall$', views.selectall, name='selectall')
 ]
--- sqltrainapp/views.py
+++ sqltrainapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    value = True
+    content = {
+        'key':value
+    }
+    return render(request, 'sqltrainapp/index.html', content)
+
+def basic(request):
+    content = {
+        'key': 1
+    }
+    return render(request, 'sqltrainapp/questions/basic/basic.html', content)
+
+def selectall(request):
+    content = {
+        'key': 1
+    }
+    return render(request, 'sqltrainapp/questions/basic/selectall.html', content)
